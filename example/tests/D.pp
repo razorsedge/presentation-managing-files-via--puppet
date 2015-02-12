62a6e84facd8362ef5a75ba761f3dac52c18ce15
Michael Arnold <github@razorsedge.org>
2015-02-12 10:20:56 -0700

    Make test be identical to the presenation.

--- example/tests/D.pp
+++ example/tests/D.pp
@@ -1,5 +1,7 @@
 file { '/tmp/testFileD':
   ensure  => present,
   mode    => '0644',
-  content => "Your operating system is: ${::operatingsystem} ${::operatingsystemrelease}\nYour CPU architecture is: ${::architecture}\n",
+  content => "Your operating system is: ${::operatingsystem}\
+${::operatingsystemrelease}\nYour CPU architecture\
+is: ${::architecture}\n",
 }
